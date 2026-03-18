c28ea8c43777384888243e067a43b231d95f79dc
DavidCaaveral <dcanaveral4.5+1@gmail.com>
2026-03-18 11:42:59 -0500

    clase 11 desarrollo

---
+++ Introduccion al desarrollo de software/clases/Clase 11/script.py
@@ -0,0 +1,16 @@
+# Autor: David Cañaveral
+# Version: 1.0
+# Fecha: 18/03/2026
+# Descripcion: Programa que muestre en pantalla los numeros pares del 0 al 10
+
+import time
+
+contador_pares=0 # declaración de un contador
+for numero_par in range(11):
+    if(numero_par % 2 == 0):
+        print(numero_par)
+        contador_pares += 1
+
+time.sleep(1)
+
+print(f" \nLos pares hallados fueron: {contador_pares}")        
